d0a7308a8e04d935a4554aa93030b71db40deabe
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-02-27 19:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+let input = document.getElementById('newTodoInput')
+let taskBtn = document.getElementById('addTodoBtn');
+let olTodoContainer = document.getElementById('todoList');
 
+taskBtn.addEventListener('click',addToTask)
+
+function addToTask(){
+    let taskValue = input.value
+    let list = document.createElement('li')
+    list.innerText = taskValue
+    console.log(taskValue)
+    olTodoContainer.append(list)
+    input.value = "";
+
+
+}
